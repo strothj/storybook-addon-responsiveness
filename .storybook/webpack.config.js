23844cb990f55dc99af4edb9deb0bdcb6ff9121e
jason@jasons.io <jason@jasons.io>
2018-01-15 10:41:04 -0500

    feat: simplify tsconfig

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,8 @@
-const genDefaultConfig = require("@storybook/react/dist/server/config/defaults/webpack.config.js");
-
 module.exports = (config, env) => {
   config.module.rules.push({
     test: /\.tsx?/,
     exclude: /node_modules/,
-    use: [
-      {
-        loader: "ts-loader",
-        options: {
-          configFile: "tsconfig.storybook.json",
-        },
-      },
-    ],
+    loader: "ts-loader",
   });
   config.resolve.extensions.push(".tsx", ".ts");
 
